31e065f5cd6a4176e7a6e1eac1d3d7fece1984d7
Narendra-Rokkam <narendrarokkam229@gmail.com>
2024-10-20 16:29:15 +0530

    add the files for code deploy

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,4 +5,4 @@ set -e
 docker pull narendra229/simple-python-flask-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 narendra229/simple-pyhton-flask-app
+docker run -d -p 5000:5000 narendra229/simple-python-flask-app
